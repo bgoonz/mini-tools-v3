21ef5260484f5f0bb3aa7ee6e62ff816c53918cf
Bryan C Guner <bryan.guner@gmail.com>
2021-12-17 11:25:28 +0000

    -update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import TimestampConverter from "@tools/TimestampConverter";
